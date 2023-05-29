28f6bbff5ba6716902f121306c862b46841709eb
twinklose <mathieu.dacheux@icloud.com>
2023-05-29 21:22:20 +0200

    New theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { router } from './routes/routes.ts'
 import { RouterProvider } from 'react-router-dom'
+import theme from './assets/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<RouterProvider router={router} />
 		</ChakraProvider>
 	</React.StrictMode>
