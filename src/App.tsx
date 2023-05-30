86762156881dfa4b8386da456f1bcb80799840d2
twinklose <mathieu.dacheux@icloud.com>
2023-05-30 21:25:23 +0200

    Removing all files from ChrakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
 import { router } from './routes/routes.ts'
 import { RouterProvider } from 'react-router-dom'
-import theme from './assets/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
