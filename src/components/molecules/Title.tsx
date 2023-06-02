81895f25e00af43de824349c34fbcfdcf955e0d0
twinklose <mathieu.dacheux@icloud.com>
2023-06-02 16:59:52 +0200

    Add delay

--- src/components/molecules/Title.tsx
+++ src/components/molecules/Title.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Typography from '../atoms/Typography'
 import { TitleBlock } from './styles/style'
+import { useTrail, animated } from '@react-spring/web'
 
 export default function Title(): JSX.Element {
+  const trail = useTrail(2, {
+    from: { opacity: 0, transform: 'translate3d(0, 100%, 0)' },
+    to: { opacity: 1, transform: 'translate3d(0, 0%, 0)' },
+    delay: 1000,
+  })
   return (
     <TitleBlock>
-      <Typography variant="h1" content="Mathieu" />
-      <Typography variant="h2" content="Dacheux" />
+      {trail.map((props, index) => (
+        <animated.div key={index} style={props}>
+          {index === 0 ? (
+            <Typography variant="h1" content="Mathieu" />
+          ) : (
+            <Typography variant="h2" content="Dacheux" />
+          )}
+        </animated.div>
+      ))}
     </TitleBlock>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,6 +13,7 @@ export default function Home({
   const trails = useTrail(1, {
     from: { opacity: 0, transform: 'translate3d(0, 100%, 0)' },
     to: { opacity: 1, transform: 'translate3d(0, 0%, 0)' },
+    delay: 1000,
   })
   return (
     <>
