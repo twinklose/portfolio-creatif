80f86a6698960d8f185e00d94608796450f8aee6
twinklose <mathieu.dacheux@icloud.com>
2023-06-03 12:18:45 +0200

    Improvement on routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,11 @@ import About from './pages/About'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Projects from './pages/Projects'
-import { useLocation, useRoutes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Global from './components/styles/global'
 import Error from './pages/Error'
-import { AnimatePresence } from 'framer-motion'
 
-export default function App(): JSX.Element | null {
+export default function App(): JSX.Element {
   if (!window.localStorage.getItem('theme')) {
     window.localStorage.setItem('theme', 'light')
   }
@@ -20,25 +19,26 @@ export default function App(): JSX.Element | null {
   const { theme, themeToggler } = useThemeMode()
   const themeMode = theme === 'light' ? lightTheme : darkTheme
 
-  const routeElements = useRoutes([
-    { path: '/', element: <Home themeToggler={themeToggler} /> },
-    { path: '/about', element: <About themeToggler={themeToggler} /> },
-    { path: '/contact', element: <Contact themeToggler={themeToggler} /> },
-    { path: '/projects', element: <Projects themeToggler={themeToggler} /> },
-    { path: '*', element: <Error /> },
-  ])
-
-  const location = useLocation()
-
-  if (!routeElements) return null
-
   return (
     <DarkModeContext>
       <ThemeProvider theme={themeMode}>
         <Global />
-        <AnimatePresence mode="wait">
-          {React.cloneElement(routeElements, { key: location.pathname })}
-        </AnimatePresence>
+        <Routes>
+          <Route index element={<Home themeToggler={themeToggler} />} />
+          <Route
+            path="/about"
+            element={<About themeToggler={themeToggler} />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact themeToggler={themeToggler} />}
+          />
+          <Route
+            path="/projects"
+            element={<Projects themeToggler={themeToggler} />}
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </ThemeProvider>
     </DarkModeContext>
   )
